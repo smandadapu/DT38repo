2fefd8626deb85ae392e4d8ce48f6f1b1a902839
smandadapu2 <smandadapu2@dxc.com>
2020-05-13 08:58:42 +0530

    scripting filter implement

---
+++ CrossScriptingFilter.java
@@ -0,0 +1,36 @@
+package com.csc.cscip.ux.common.security.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+
+
+public class CrossScriptingFilter implements Filter {
+
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request), response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ UXCLTTokenUtils.java
@@ -0,0 +1,37 @@
+package com.csc.cscip.ux.common.security.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Hex;
+
+public class UXCLTTokenUtils {
+	public static final String MAGIC_KEY = "obfuscate";
+	public static String createToken(String username, long expires) {
+
+		StringBuilder tokenBuilder = new StringBuilder();
+		tokenBuilder.append(username);
+		tokenBuilder.append("-");
+		tokenBuilder.append(expires);
+		tokenBuilder.append("-");
+		tokenBuilder.append(UXCLTTokenUtils.computeSignature_PT(username, expires));
+		return tokenBuilder.toString();
+	}
+
+	public static String computeSignature_PT(String username, long expires) {
+
+		StringBuilder signatureBuilder = new StringBuilder();
+		signatureBuilder.append(username);
+		signatureBuilder.append("-");
+		signatureBuilder.append(expires);
+		signatureBuilder.append("-");
+		signatureBuilder.append(UXCLTTokenUtils.MAGIC_KEY);
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalStateException("No MD5 algorithm available!");
+		}
+		return new String(Hex.encodeHexString(digest.digest(signatureBuilder.toString().getBytes())));
+	}
+}
